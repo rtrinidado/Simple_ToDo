d50de6e70d37abd0a160f35ac2be00a8f62b3c97
Raul Trinidad <raulto9351@gmail.com>
2021-12-04 17:09:44 -0600

    DataBinding en RecyclerView

--- app/src/main/java/com/raultorinz/simpletodo/adapter/RecyclerAdapter.kt
+++ app/src/main/java/com/raultorinz/simpletodo/adapter/RecyclerAdapter.kt
@@ -9,10 +9,10 @@ import android.view.ViewGroup
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.raultorinz.simpletodo.R
+import com.raultorinz.simpletodo.databinding.TodoElementLayoutBinding
 import com.raultorinz.simpletodo.room.Task
 import com.raultorinz.simpletodo.ui.main.MainFragmentDirections
 import com.raultorinz.simpletodo.ui.main.MainViewModel
-import kotlinx.android.synthetic.main.todo_element_layout.view.*
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.*
@@ -21,28 +21,22 @@ class RecyclerAdapter(private val viewModel: MainViewModel, private val context:
         RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
     private var taskList: List<Task>? = null
 
-    inner class ViewHolder(itemView: View, viewModel: MainViewModel) :
-            RecyclerView.ViewHolder(itemView) {
-        var check = itemView.radioButton
-        var name = itemView.toDoText
-        var date = itemView.toDoDate
-        var delete = itemView.deleteButton
-        var id: Long = 0
+    inner class ViewHolder(val binding: TodoElementLayoutBinding, viewModel: MainViewModel) :
+        RecyclerView.ViewHolder(binding.root) {
+        var name = binding.toDoText
 
-        init {
-            itemView.setOnClickListener {
-                var action = MainFragmentDirections.mainFragmentToAddTaskFragment()
-                action.idTask = id
-                Navigation.findNavController(it).navigate(action)
-            }
-
-            check.setOnClickListener {
-                viewModel.updateTaskStatus(id, check.isChecked)
-            }
-            delete.setOnClickListener {
-                viewModel.deleteTask(id)
-            }
+        fun bind(viewModel: MainViewModel, item: Task) {
+            binding.viewModel = viewModel
+            binding.task = item
+            binding.adapter = this@RecyclerAdapter
+            binding.executePendingBindings()
         }
+    }
+
+    fun seeTask(id: Long, view: View) {
+        val action = MainFragmentDirections.mainFragmentToAddTaskFragment()
+        action.idTask = id
+        Navigation.findNavController(view).navigate(action)
     }
 
     fun setTaskList(tasks: List<Task>) {
@@ -55,33 +49,24 @@ class RecyclerAdapter(private val viewModel: MainViewModel, private val context:
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val v =
-            LayoutInflater.from(parent.context).inflate(R.layout.todo_element_layout, parent, false)
-        return ViewHolder(v, viewModel)
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = TodoElementLayoutBinding.inflate(layoutInflater, parent, false)
+        return ViewHolder(binding, viewModel)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         taskList.let {
-            holder.check.isChecked = it!![position].completed
-            holder.name.text = it[position].name
-            holder.name.paintFlags = if (it[position].completed) Paint.STRIKE_THRU_TEXT_FLAG else Paint.HINTING_OFF
-            holder.date.text = it[position].dateTask
-            if (it[position].dateTask.isNullOrEmpty()) {
-                holder.date.visibility = View.GONE
-            } else {
-                holder.date.setTextColor(checkDate(it[position].dateTask!!, it[position].completed))
-                holder.date.visibility = View.VISIBLE
-            }
-            holder.id = it[position].id
+            holder.bind(viewModel, it?.get(position) ?: Task())
+            holder.name.paintFlags =
+                if (it?.get(position)?.completed == true) Paint.STRIKE_THRU_TEXT_FLAG else Paint.HINTING_OFF
         }
     }
 
     override fun getItemCount(): Int {
-        return if (taskList == null) 0 else taskList!!.size
+        return taskList?.size ?: 0
     }
 
     private fun getDate(dateS: String): LocalDate {
-
         val format = context.getString(R.string.date_format)
         val local = Locale("es", "MX")
         val formatter = DateTimeFormatter.ofPattern(format, local)
@@ -91,10 +76,13 @@ class RecyclerAdapter(private val viewModel: MainViewModel, private val context:
             LocalDate.MIN
     }
 
-    private fun checkDate(dateS: String, checked: Boolean): Int {
-        val today = LocalDate.now()
-        val date = getDate(dateS)
-        val comparison = today.compareTo(date)
+    fun checkDate(dateS: String?, checked: Boolean): Int {
+        var comparison = -1
+        if (!dateS.isNullOrEmpty()) {
+            val today = LocalDate.now()
+            val date = getDate(dateS)
+            comparison = today.compareTo(date)
+        }
         return when {
             checked -> context.getColor(R.color.blue_grotto)
             comparison > 0 -> context.getColor(R.color.rose_red)
